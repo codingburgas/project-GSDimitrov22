7475b9336504f86f49ce2a18dc0a0b30bb92e223
GSDimitrov22 <GSDimitrov22@codingburgas.bg>
2026-04-17 17:21:15 +0300

    Add all controller

---
+++ WebApplication1/WebApplication1/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarpoolingApp.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/TripController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarpoolingApp.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
